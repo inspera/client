b2da97fc38f31862e8776dd298f7dd66f5fbb28e
Alex Cipri <ciprian.grosan@exswap.com>
2021-05-19 10:32:40 +0300

    IA1-4554 - added separate captions

--- src/annotator/adder.js
+++ src/annotator/adder.js
@@ -286,7 +286,7 @@ export class Adder {
     const existingTools = [
       {
         name: 'annotate',
-        label: this._captions.create + ' ' + this._captions.annotate || 'create annotate',
+        label: this._captions.annotateLabel || 'create annotation',
         caption: this._captions.annotate || 'annotate',
         command: () => {
           this._onAnnotate();
@@ -296,7 +296,7 @@ export class Adder {
       },
       {
         name: 'highlight',
-        label: this._captions.create + ' ' + this._captions.highlight  || 'create highlight',
+        label: this._captions.highlightLabel  || 'create highlight',
         caption: this._captions.highlight || 'highlight',
         command: () => {
           this._onHighlight();
